6969d2f4e5bfbccde5d9733148a7708246e3c342
Meghin Mather <megmath@pdx.edu>
2026-03-19 12:26:00 -0700

    created some viz plots

--- R/Intro_data_summary.R
+++ R/Intro_data_summary.R
@@ -17,3 +17,26 @@ summary(raw_15min)
 glimpse(raw_15min)
 dfSummary(raw_15min)
 
+
+mean(raw_15min$speed)
+mean(raw_15min$volume)
+hist(raw_15min$volume)
+
+# learn about filters
+occ_20plus <- raw_15min |>
+  filter(occupancy == 20)  
+
+occ10_sp80 <- raw_15min |> 
+    filter(occupancy < 10 & speed > 80)
+table(occ10_sp80$detector_id)
+
+# working with NAs
+blank_example <- raw_15min |> 
+  filter(is.na(speed))
+complete_df <- raw_15min |> 
+  filter(!is.na(speed))
+
+subset_o10s80 <- occ10_sp80 |> 
+  filter(detector_id %in% c(101105, 101176))
+
+saveRDS(complete_df, "data/clean_data.rds")
